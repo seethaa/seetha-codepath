6749b81be1b8ccf3c44146ad82a64898923cd84e
Seetha Annamraju <seetha@Seethas-MBP.home>
2016-06-21 21:56:29 -0400

    SimpleTodo app basic code. Handles add/remove items, tap item to edit task, and persist items using text file.

---
+++ app/src/main/java/com/codepath/simpletodo/MainActivity.java
@@ -0,0 +1,106 @@
+package com.codepath.simpletodo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<String> todoItems;
+    ArrayAdapter<String> aToDoAdapter;
+    ListView lvItems;
+    EditText etEditText;
+    private final int REQUEST_CODE = 22;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        todoItems = new ArrayList<String>();
+        populateArrayItems();
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        lvItems.setAdapter(aToDoAdapter);
+        etEditText = (EditText) findViewById(R.id.etEditText);
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                todoItems.remove(position);
+                aToDoAdapter.notifyDataSetChanged();
+                writeItems();
+                return true;
+            }
+        });
+
+        lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+                Intent i = new Intent(MainActivity.this, EditItemActivity.class);
+                String text = todoItems.get(position);
+                // put "extras" into the bundle for access in the second activity
+                i.putExtra("text", text);
+                i.putExtra("position", position);
+                // brings up the second activity
+                startActivityForResult(i, REQUEST_CODE);
+
+            }
+        });
+    }
+
+    public void populateArrayItems(){
+        readItems();
+        aToDoAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, todoItems);
+    }
+
+    private void readItems(){
+        File filesDir = getFilesDir();
+        File file = new File(filesDir, "todo.txt");
+        try{
+            todoItems = new ArrayList<String>(FileUtils.readLines(file));
+        } catch (IOException e){
+
+        }
+    }
+
+    private void writeItems(){
+        File filesDir = getFilesDir();
+        File file = new File(filesDir, "todo.txt");
+        try{
+            FileUtils.writeLines(file, todoItems);
+        } catch (IOException e){
+
+        }
+    }
+    public void onAddItem(View view) {
+        aToDoAdapter.add(etEditText.getText().toString());
+        etEditText.setText("");
+        writeItems();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // REQUEST_CODE is defined above
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+            // Extract name value from result extras
+            String newText = data.getExtras().getString("newtext");
+            int position = data.getExtras().getInt("position", 0);
+            todoItems.set(position, newText);
+            aToDoAdapter.notifyDataSetChanged();
+            writeItems();
+            // Toast the name to display temporarily on screen
+            Toast.makeText(this, newText, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
